ed7a63ce4cfac26ed32d4953afbe841869233bac
AKarp123 <87674424+AKarp123@users.noreply.github.com>
2021-08-04 15:50:52 -0400

    ok

--- index.js
+++ index.js
@@ -98,11 +98,26 @@ if($('#cart').length){
     var orderNo = 1;
     for(order of data){
 
-        $("#cart").append(`<p>Order #${orderNo++}</p>`)
-        $("#cart").append(`<p>${order.toppings.pepperoni} pepperoni</p>`)
-        $("#cart").append(`<p>${order.toppings.mushrooms} mushrooms</p>`)
-        $("#cart").append(`<p>${order.toppings.bacon} bacon</p>`)
-        $("#cart").append(`<p>${order.toppings.olives} olives</p>`)
+        // $("#cart").append(`<p>Order #${orderNo++}</p>`)
+        // $("#cart").append(`<p>${order.toppings.pepperoni} pepperoni</p>`)
+        // $("#cart").append(`<p>${order.toppings.mushrooms} mushrooms</p>`)
+        // $("#cart").append(`<p>${order.toppings.bacon} bacon</p>`)
+        // $("#cart").append(`<p>${order.toppings.olives} olives</p>`)
+        $("#cart").append(`
+        <div class="card receipt" style="width: 18rem;">
+            <div class="card-header">
+                    Pizza #${orderNo++}
+            </div>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">${order.toppings.pepperoni} pepperoni</li>
+            <li class="list-group-item">${order.toppings.mushrooms} mushrooms</li>
+            <li class="list-group-item">${order.toppings.bacon} bacon</li>
+            <li class="list-group-item">${order.toppings.olives} olives</li>
+            
+            </ul>
+      </div>`)
+
+        
     }
 }
 
